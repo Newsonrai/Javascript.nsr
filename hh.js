63ef435aed22f0b64a924932c3f5ba4f54062fa1
Newson Rai <firstnepseking@gmail.com>
2025-05-12 23:20:24 +0545

    examples of .filter and .reduce

---
+++ hh.js
@@ -0,0 +1,31 @@
+// //  .filter HOF
+
+// const books =[{
+//     title:"Think and Grow Rich",
+//     author:"Napoleon Hill",
+//     year:1937
+// },
+// {
+//     title:"The Power of Habit",
+//     author:"Charles Duhigg",
+//     year:2013
+// },
+// {
+//     title:"Atomic Habits",
+//     author:"James Clear",
+//     year:2018
+// },
+// {
+//     title:"Rich Dad Poor Dad",
+//     author: "Robert Kiyosaki",
+//     year:1997
+// }]
+// const output = books.filter((book)=>{
+//     // return book.year>2000
+//     return book.author === "Robert Kiyosaki" || book.author === "James Clear"
+// })
+// console.log(output)
+
+
+// .reduce hof
+
